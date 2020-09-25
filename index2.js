ddf602e43cf27e3d51f0e4a1186d1b2630ff4b3d
paigelindahl <paigemarlindahl@gmail.com>
2020-09-25 03:48:44 +0000

    working with promises

---
+++ index2.js
@@ -0,0 +1,15 @@
+const {nextISSTimesForMyLocation} =require('./iss_promised');
+
+// fetchMyIP()
+//   .then(fetchCoordsByIP)
+//   .then(fetchISSFlyOverTimes)
+//   .then(body => console.log(body));
+
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  })
+  .catch((error) => {
+    console.log('It didnt work: ', error.message);
+  });
+
